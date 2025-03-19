5aef1fcb1e237ac9edec23aa85490a02a51e7db3
blanelts <blanelts@gmail.com>
2025-03-19 15:23:52 +0300

    fix init db

--- server/init.sql
+++ server/init.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS server_stats (
     id SERIAL PRIMARY KEY,
     hostname VARCHAR(255),
-    ip_address VARCHAR(255),
-    cpu_usage DOUBLE PRECISION,
+    ip_address VARCHAR(50),
+    cpu_usage REAL,
     memory_used INT,
     memory_total INT,
     disk_free INT,
-    disk_total INT
+    disk_total INT,
+    os_info VARCHAR(100),
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-ALTER TABLE server_stats ADD COLUMN last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE server_stats ADD COLUMN os_info VARCHAR(100);
 
